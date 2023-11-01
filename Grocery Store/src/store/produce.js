67bbac5a20b32ecaf43c6492bf143db886c34381
Jason <jasonzhang501@gmail.com>
2023-11-01 14:21:19 -0400

    Up to End Of P2

---
+++ Grocery Store/src/store/produce.js
@@ -0,0 +1,22 @@
+import produceData from '../mockData/produce.json'
+
+export default function produceReducer(state = {}, action) {
+    switch (action.type) {
+      case POPULATE: 
+        const newState = {};
+        action.produce.forEach(produce => {
+            newState[produce.id] = produce;
+        })
+        return newState;
+      default:
+        return state;
+    }
+
+}
+
+const POPULATE = 'produce/POPULATE'
+
+export const populateProduce = () => ({
+    type: POPULATE,
+    produce: produceData
+});
